321a59da29a49c5e3f41fd8b3827122a8bb0e45a
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2023-07-12 21:08:48 -0700

    Clean-up for Windows

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,6 +1,12 @@
 #include "Game.hpp"
+
+#if defined(_WIN32)
+#include <SDL.h>
+#include <SDL_image.h>
+#else
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#endif
 #include <glm/glm.hpp>
 #include <iostream>
 
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,11 +1,11 @@
 #include "Game.hpp"
 
-int main() {
+int main(int argc, char* argv[]) {
     Game game;
 
     game.Initialize();
     game.Run();
     game.Destroy();
-    
+
     return 0;
 }
