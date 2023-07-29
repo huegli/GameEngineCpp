68fbafb33583742eb74c1e8938adc9d7abdeb299
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2023-07-28 17:26:19 -0700

    erase-remove idiom

--- Makefile
+++ Makefile
@@ -11,7 +11,8 @@ COMPILER_FLAGS = -Wall -Wfatal-errors
 INCLUDE_PATH = -I"./libs" -I"./libs/lua"
 SRC_FILES = src/*.cpp \
 			src/Game/*.cpp \
-			src/Logger/*.cpp 
+			src/Logger/*.cpp \
+ 			src/ECS/*.cpp
 LINKER_FLAGS = -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer -llua5.4 $(SDL2_LIB_PATH)
 OBJ_NAME = gameengine 
 
--- src/ECS/ECS.cpp
+++ src/ECS/ECS.cpp
@@ -1,6 +1,34 @@
 #include "ECS.hpp"
 
+#include<vector>
+#include<algorithm>
+
 int Entity::GetId() const {
   return id;
 }
 
+void System::AddEntityToSystem(Entity entity) {
+  // check if entity already exists and return if so
+  // auto it = std::find(entities.begin(), entities.end(), entity);
+
+  // if (it == entities.end()) {
+
+  // }
+
+  // add entity to the end of the vector
+  entities.push_back(entity);
+}
+
+void System::RemoveEntityFromSystem(Entity entity) {
+  entities.erase(std::remove_if(entities.begin(), entities.end(), [&entity](Entity other) {
+    return entity.GetId() == other.GetId();
+  }));
+}
+
+std::vector<Entity> System::GetSystemEntities() const {
+  return entities;
+}
+
+const Signature& System::GetComponentSignature() const {
+  return componentSignature;
+}
